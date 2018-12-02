0e8c5bedabfc62899ecba960d58a46b22fdb1573
wojtas <kefiras@gmail.com>
2018-12-02 22:08:47 +0100

    Added EC2 loop and custom filter

---
+++ filter_plugins/nested_lists.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from ansible import errors
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'nested_lists' : self.nested_lists,
+        }
+
+    def nested_lists(self, data):
+        """
+        It takes the input list and returns a list pairs:
+        [{type, public_ip}]
+        """
+        if not isinstance(data, list):
+            raise errors.AnsibleFilterError("A list is expected")
+    
+        retval = []
+        for external in data:
+            for internal in external['instances']:
+                retval.append({'type': internal['tags']['type'], 'public_ip': internal['public_ip']})
+        
+        return retval
+    
+    
